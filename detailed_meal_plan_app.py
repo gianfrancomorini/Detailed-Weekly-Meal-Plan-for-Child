a889f85530af536c78852ea9e2068f0b58e94f40
gianfrancomorini <115338984+gianfrancomorini@users.noreply.github.com>
2023-11-01 14:41:55 -0700

    Update detailed_meal_plan_app.py

--- detailed_meal_plan_app.py
+++ detailed_meal_plan_app.py
@@ -1,23 +1,29 @@
 import streamlit as st
 import random
 
-# Predefined meals and ingredients (example data)
+# Expanded list of meals and ingredients (example data, expand to 50 meals as needed)
 meals = {
+    # [First few meals for example, continue this pattern to reach 50 meals]
     'Mac & Cheese': ['macaroni', 'cheese', 'milk', 'butter'],
     'Chicken Nuggets': ['chicken', 'breadcrumbs', 'eggs'],
     'Spaghetti Bolognese': ['spaghetti', 'ground beef', 'tomato sauce', 'onion', 'garlic'],
     'Grilled Cheese Sandwich': ['bread', 'cheese', 'butter'],
     'Veggie Pizza': ['pizza dough', 'tomato sauce', 'cheese', 'bell peppers', 'mushrooms'],
+    # ... add more meals here up to the 50th meal
 }
 
 # Allergens to be excluded
 allergens = {
-    'Dairy': ['cheese', 'milk', 'butter'],
-    'Gluten': ['macaroni', 'breadcrumbs', 'spaghetti', 'bread', 'pizza dough'],
-    'Eggs': ['eggs'],
+    'Dairy': ['cheese', 'milk', 'butter', 'yogurt', 'cream'],
+    'Gluten': ['macaroni', 'breadcrumbs', 'spaghetti', 'bread', 'pizza dough', 'flour', 'cereal'],
+    'Eggs': ['eggs', 'mayonnaise', 'some pastas'],
+    'Nuts': ['peanuts', 'almonds', 'cashews', 'nut butters'],
+    'Soy': ['soy sauce', 'tofu', 'soy milk', 'edamame'],
+    'Seafood': ['fish', 'shrimp', 'lobster', 'crab'],
+    # ... add more allergenic ingredients here if needed
 }
 
-# Helper function to filter meals based on allergens
+# Function to filter meals based on allergens
 def filter_meals(allergens_to_exclude):
     filtered_meals = {}
     for meal, ingredients in meals.items():
@@ -25,10 +31,10 @@ def filter_meals(allergens_to_exclude):
             filtered_meals[meal] = ingredients
     return filtered_meals
 
-# Helper function to create Amazon links
-def create_amazon_links(ingredients):
-    amazon_base_url = "https://www.amazon.com/s?k={}&tag=YOUR_AFFILIATE_TAG"
-    return {ingredient: amazon_base_url.format(ingredient.replace(' ', '+')) for ingredient in ingredients}
+# Function to create Amazon links with affiliate tag
+def create_amazon_links(ingredients, affiliate_tag):
+    amazon_base_url = "https://www.amazon.com/s?k={}&tag="
+    return {ingredient: amazon_base_url.format(ingredient.replace(' ', '+')) + affiliate_tag for ingredient in ingredients}
 
 # Streamlit UI
 st.title("Weekly Meal Planner for Kids")
@@ -45,23 +51,26 @@ if st.button('Generate Meal Plan'):
     # Filter meals based on selected allergens
     available_meals = filter_meals(allergens_to_exclude)
 
-    # Randomly select a meal for each day of the week
-    weekly_meal_plan = random.sample(list(available_meals.keys()), 7)
+    # Check if there are enough meals to generate a plan
+    if len(available_meals) < 7:
+        st.error("Not enough meals available to generate a full week plan based on selected allergens. Please select fewer restrictions.")
+    else:
+        # Randomly select a meal for each day of the week
+        weekly_meal_plan = random.sample(list(available_meals.keys()), 7)
 
-    # Display the meal plan
-    st.subheader("Your Weekly Meal Plan:")
-    for day, meal in zip(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"], weekly_meal_plan):
-        st.markdown(f"**{day}:** {meal}")
+        # Display the meal plan
+        st.subheader("Your Weekly Meal Plan:")
+        for day, meal in zip(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"], weekly_meal_plan):
+            st.markdown(f"**{day}:** {meal}")
 
-    # Display Amazon links for ingredients
-    st.subheader("Buy Ingredients:")
-    all_ingredients = set(sum([available_meals[meal] for meal in weekly_meal_plan], []))
-    amazon_links = create_amazon_links(all_ingredients)
-    for ingredient in all_ingredients:
-        st.markdown(f"[{ingredient}]({amazon_links[ingredient]})")
+        # Display Amazon links for ingredients
+        st.subheader("Buy Ingredients:")
+        all_ingredients = set(sum([available_meals[meal] for meal in weekly_meal_plan], []))
+        amazon_links = create_amazon_links(all_ingredients, 'gfm0dd-20')
+        for ingredient in all_ingredients:
+            st.markdown(f"[{ingredient}]({amazon_links[ingredient]})")
 
-# Run the following command in your terminal to install streamlit if you haven't already:
-# pip install streamlit
+# The rest of the app remains unchanged
 
-# To run the app, save this script as `meal_planner_app.py` and run the command:
+# Save this script as `meal_planner_app.py` and run it with Streamlit using the following command:
 # streamlit run meal_planner_app.py

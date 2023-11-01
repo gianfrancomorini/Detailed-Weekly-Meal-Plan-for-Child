dd6cc1694cccea48b7012b4526319abd7265987c
gianfrancomorini <115338984+gianfrancomorini@users.noreply.github.com>
2023-11-01 15:12:28 -0700

    Update detailed_meal_plan_app.py

--- detailed_meal_plan_app.py
+++ detailed_meal_plan_app.py
@@ -4,15 +4,63 @@ import random
 # Define a dictionary of meals with their explanations and ingredients
 meals = {
     "Pasta with Tomato Sauce": {
-        "explanation": "Boil pasta and top with homemade tomato sauce.",
+        "explanation": "Boil pasta until al dente, drain, and mix with warmed tomato sauce. Serve with grated Parmesan cheese on top.",
         "ingredients": ["Pasta", "Tomato sauce", "Parmesan cheese"]
     },
     "Grilled Cheese Sandwich": {
-        "explanation": "Grill bread with cheese until golden brown.",
-        "ingredients": ["Bread", "Cheese", "Butter"]
+        "explanation": "Butter the outside of two slices of bread, place a slice of cheese between them, and grill on a skillet until each side is golden brown.",
+        "ingredients": ["Bread", "Cheddar cheese", "Butter"]
     },
-    # ... Add more meals as needed
+    "Chicken Nuggets with Veggies": {
+        "explanation": "Bake chicken nuggets according to package instructions and serve with steamed carrots and broccoli.",
+        "ingredients": ["Chicken nuggets", "Carrots", "Broccoli"]
+    },
+    "Tuna Salad": {
+        "explanation": "Mix canned tuna with mayonnaise, chopped celery, and a squeeze of lemon. Serve on a bed of lettuce or as a sandwich filling.",
+        "ingredients": ["Canned tuna", "Mayonnaise", "Celery", "Lemon", "Lettuce"]
+    },
+    "Peanut Butter and Jelly Sandwich": {
+        "explanation": "Spread peanut butter on one slice of bread and jelly on the other. Press together and cut into halves or quarters.",
+        "ingredients": ["Bread", "Peanut butter", "Jelly"]
+    },
+    "Macaroni and Cheese": {
+        "explanation": "Prepare macaroni pasta, mix with cheese sauce until creamy and well combined. Optionally, bake for a crispy top layer.",
+        "ingredients": ["Macaroni pasta", "Cheese", "Milk", "Butter"]
+    },
+    "Scrambled Eggs and Toast": {
+        "explanation": "Whisk eggs with salt and pepper, scramble on a skillet until cooked. Serve with buttered toast.",
+        "ingredients": ["Eggs", "Salt", "Pepper", "Bread", "Butter"]
+    },
+    "Ham and Cheese Quesadilla": {
+        "explanation": "Place slices of ham and cheese between two tortillas, cook on a skillet until the tortillas are crispy and the cheese is melted.",
+        "ingredients": ["Tortillas", "Ham", "Cheddar cheese"]
+    },
+    "Vegetable Stir Fry": {
+        "explanation": "Stir-fry a mix of vegetables such as bell peppers, broccoli, and carrots in a wok with soy sauce and serve over rice.",
+        "ingredients": ["Bell peppers", "Broccoli", "Carrots", "Soy sauce", "Rice"]
+    },
+    "French Toast": {
+        "explanation": "Dip slices of bread into a mixture of beaten eggs, milk, and a pinch of cinnamon, then fry until golden brown. Serve with maple syrup.",
+        "ingredients": ["Bread", "Eggs", "Milk", "Cinnamon", "Maple syrup"]
+    },
+    "Fruit Salad": {
+        "explanation": "Chop a variety of fruits such as apples, bananas, and grapes and mix them together for a fresh fruit salad.",
+        "ingredients": ["Apples", "Bananas", "Grapes"]
+    },
+    "Beef Tacos": {
+        "explanation": "Brown ground beef with taco seasoning, serve in taco shells with shredded lettuce, cheese, and salsa.",
+        "ingredients": ["Ground beef", "Taco seasoning", "Taco shells", "Lettuce", "Cheddar cheese", "Salsa"]
+    },
+    "Baked Fish Sticks": {
+        "explanation": "Bake fish sticks according to the package instructions and serve with a side of green beans and tartar sauce.",
+        "ingredients": ["Fish sticks", "Green beans", "Tartar sauce"]
+    },
+    "Homemade Pizza": {
+        "explanation": "Top a pizza base with tomato sauce, shredded cheese, and your choice of toppings. Bake until the crust is crispy and the cheese is bubbly.",
+        "ingredients": ["Pizza base", "Tomato sauce", "Mozzarella cheese", "Pepperoni"]
+    }
 }
+
 
 # Function to create an Amazon link with a referral tag
 def create_amazon_link(ingredient):

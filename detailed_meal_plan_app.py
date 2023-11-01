f6271a6fa7b7b9b1d8e5314e7ddaf1db58cd8b19
gianfrancomorini <115338984+gianfrancomorini@users.noreply.github.com>
2023-11-01 13:32:54 -0700

    Update detailed_meal_plan_app.py

--- detailed_meal_plan_app.py
+++ detailed_meal_plan_app.py
@@ -1,52 +1,33 @@
-# Streamlit Weekly Meal Plan App with Detailed Meals and Snacks
+# Streamlit Weekly Meal Plan App with Dynamic Images
 import streamlit as st
 import random
+import requests
 
-# Sample meals, snacks, their preparation instructions, and ingredients
+# Your Amazon affiliate code
+affiliate_code = "gfm0dd-20"  
+
+# Using Unsplash API to fetch images
+def get_image_url(query):
+    endpoint = "https://api.unsplash.com/search/photos"
+    headers = {
+        "Authorization": "Client-ID y7sY9svzrBEGTMLfN4mFwjjoP_aP11y7lZcLqH7Ml-Q"
+    }
+    params = {
+        "query": query,
+        "per_page": 1
+    }
+    response = requests.get(endpoint, headers=headers, params=params)
+    data = response.json()
+    if data['results']:
+        return data['results'][0]['urls']['small']
+
+# Sample structure of meals
 meals = {
-    "Spaghetti with Marinara Sauce": {
-        "instructions": "Boil spaghetti until al dente. Heat marinara sauce in a pan and mix with spaghetti. Serve with parmesan cheese on top.",
-        "ingredients": ["spaghetti", "marinara sauce", "parmesan cheese"]
+    "Meal 1": {
+        "instructions": ["Step 1", "Step 2"],
+        "ingredients": ["ingredient 1", "ingredient 2"]
     },
-    "Grilled Cheese Sandwich": {
-        "instructions": "Butter one side of each bread slice. Place cheese between bread slices, buttered side out. Grill on a pan until golden brown.",
-        "ingredients": ["bread", "cheese", "butter"]
-    },
-    "Chicken Nuggets with Mashed Potatoes": {
-        "instructions": "Bake chicken nuggets as per package instructions. Boil potatoes until soft, mash with butter and milk. Serve together.",
-        "ingredients": ["chicken nuggets", "potatoes", "butter", "milk"]
-    },
-    "Mac and Cheese": {
-        "instructions": "Boil macaroni until al dente. In a separate pan, melt butter, add milk and cheese. Mix sauce with macaroni.",
-        "ingredients": ["macaroni", "cheddar cheese", "milk", "butter"]
-    },
-    "Vegetable Stir-fry": {
-        "instructions": "Stir-fry broccoli and bell peppers in a pan. Add tofu and soy sauce. Cook until vegetables are tender.",
-        "ingredients": ["broccoli", "bell peppers", "soy sauce", "tofu"]
-    },
-    "Cheese Pizza": {
-        "instructions": "Spread tomato sauce on pizza dough. Top with mozzarella cheese. Bake until crust is golden brown.",
-        "ingredients": ["pizza dough", "tomato sauce", "mozzarella cheese"]
-    }
-}
-
-snacks = {
-    "Fruit Salad": {
-        "instructions": "Dice a variety of fruits and mix them together in a bowl.",
-        "ingredients": ["apple", "banana", "orange"]
-    },
-    "Yogurt with Granola": {
-        "instructions": "Pour yogurt into a bowl and top with granola.",
-        "ingredients": ["yogurt", "granola"]
-    },
-    "Peanut Butter & Jelly Sandwich": {
-        "instructions": "Spread peanut butter and jelly on bread slices and serve.",
-        "ingredients": ["bread", "peanut butter", "jelly"]
-    },
-    "Cheese Sticks": {
-        "instructions": "Take cheese sticks from packaging and serve.",
-        "ingredients": ["cheese sticks"]
-    }
+    # ... (Repeat the structure for 1000 meals)
 }
 
 def generate_amazon_link(ingredient, affiliate_code):
@@ -54,29 +35,42 @@ def generate_amazon_link(ingredient, affiliate_code):
     base_url = f"https://www.amazon.com/s?k={ingredient.replace(' ', '+')}&tag={affiliate_code}"
     return base_url
 
-affiliate_code = "gfm0dd-20"  # Your Amazon affiliate code
-
 st.title('Detailed Weekly Meal Plan for Child')
+st.write("Please specify any ingredients you'd like to avoid due to allergies:")
+
+# User input for allergies
+allergy_ingredients = st.text_input("Ingredients to avoid (comma separated)").split(',')
+
+# Filter out meals that contain the allergenic ingredients
+filtered_meals = {k: v for k, v in meals.items() if not any(ingredient in v["ingredients"] for ingredient in allergy_ingredients)}
 
 if st.button('Generate Meal Plan'):
     all_ingredients = set()
+    
     for i in range(7):  # For each day of the week
         st.write(f"### Day {i + 1}")
         
-        selected_meals = random.sample(list(meals.keys()), 2)
+        selected_meals = random.sample(list(filtered_meals.keys()), 2)
         for meal in selected_meals:
             st.write(f"**Meal: {meal}**")
-            st.write(meals[meal]['instructions'])
+            
+            # Fetch image from Unsplash
+            image_url = get_image_url(meal)
+            if image_url:
+                st.image(image_url, caption=meal, use_column_width=True)
+            
+            for instruction in meals[meal]['instructions']:
+                st.write(instruction)
+            
+            st.write("Ingredients:")
+            for ingredient in meals[meal]['ingredients']:
+                st.markdown(f"[{ingredient}]({generate_amazon_link(ingredient, affiliate_code)})")
+            
             all_ingredients.update(meals[meal]['ingredients'])
+            st.write("\n")
         
-        selected_snacks = random.sample(list(snacks.keys()), 2)
-        for snack in selected_snacks:
-            st.write(f"**Snack: {snack}**")
-            st.write(snacks[snack]['instructions'])
-            all_ingredients.update(snacks[snack]['ingredients'])
+        # ... (Similar structure for snacks)
         
-        st.write("\n")
-
     st.write("### Amazon links to buy ingredients:")
     for ingredient in all_ingredients:
         st.write(f"{ingredient}: {generate_amazon_link(ingredient, affiliate_code)}")

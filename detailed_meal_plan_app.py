dfba33275b1cbd44698bb6afffc52e7414ded756
gianfrancomorini <115338984+gianfrancomorini@users.noreply.github.com>
2023-11-01 15:07:10 -0700

    Update detailed_meal_plan_app.py

--- detailed_meal_plan_app.py
+++ detailed_meal_plan_app.py
@@ -1,52 +1,48 @@
 import streamlit as st
 
-# This is a placeholder for your Amazon referral link
-AMAZON_REFERRAL = "gfm0dd-20"
+# Placeholder for your Amazon referral link
+AMAZON_REFERRAL_TAG = "gfm0dd-20"
 
-# Sample meal data
-meals = {
-    "Monday": {
-        "Breakfast": {
-            "Name": "Oatmeal with Fruits",
-            "Preparation": "Cook the oatmeal and top it with your choice of fruits.",
-            "Ingredients": ["Oatmeal", "Banana", "Strawberry", "Honey"]
+def create_amazon_search_link(ingredient):
+    # URL encode the ingredient name for the Amazon URL
+    return f"https://www.amazon.com/s?k={ingredient.replace(' ', '+')}&tag={AMAZON_REFERRAL_TAG}"
+
+def generate_weekly_meals():
+    # This function would ideally generate a dynamic meal plan or retrieve it from a database
+    weekly_meals = {
+        'Monday': {
+            'Breakfast': {
+                'Name': 'Scrambled Eggs with Toast',
+                'Preparation': 'Scramble eggs and serve with a side of toasted bread.',
+                'Ingredients': ['Eggs', 'Bread', 'Butter', 'Salt', 'Pepper']
+            },
+            'Dinner': {
+                'Name': 'Baked Salmon with Vegetables',
+                'Preparation': 'Bake salmon and steam vegetables.',
+                'Ingredients': ['Salmon fillets', 'Broccoli', 'Carrots', 'Olive oil', 'Lemon']
+            }
         },
-        "Dinner": {
-            "Name": "Grilled Chicken Salad",
-            "Preparation": "Grill the chicken and mix it with salad greens and dressing.",
-            "Ingredients": ["Chicken breast", "Salad greens", "Caesar dressing"]
-        }
-    },
-    # Add similar dictionaries for other days of the week
-}
+        # ... Add entries for other days of the week
+    }
+    return weekly_meals
 
-def generate_meal_plan():
-    # This function would generate a meal plan, for simplicity we're using a static dict
-    return meals
-
-def create_amazon_link(ingredient):
-    # This function creates a URL for an ingredient search on Amazon with the referral tag
-    return f"https://www.amazon.com/s?k={ingredient.replace(' ', '+')}&tag={AMAZON_REFERRAL}"
-
-def display_meal(day, meal_type, meal):
-    st.header(f"{day} - {meal_type}")
-    st.subheader(meal["Name"])
-    st.write(meal["Preparation"])
-    st.subheader("Ingredients")
-    for ingredient in meal["Ingredients"]:
-        link = create_amazon_link(ingredient)
-        st.markdown(f"- {ingredient} ([Buy on Amazon]({link}))")
+def display_meals(weekly_meals):
+    for day, meals in weekly_meals.items():
+        st.header(day)
+        for meal_time, meal_info in meals.items():
+            st.subheader(meal_info['Name'] + f" ({meal_time})")
+            st.write("Preparation:", meal_info['Preparation'])
+            st.write("Ingredients:")
+            for ingredient in meal_info['Ingredients']:
+                link = create_amazon_search_link(ingredient)
+                st.markdown(f"- {ingredient} ([Buy on Amazon]({link}))")
 
 def app():
     st.title("Child Weekly Meal Calendar")
-
-    # Generate or retrieve the meal plan
-    weekly_meal_plan = generate_meal_plan()
-
-    # Display the meal plan
-    for day, day_meals in weekly_meal_plan.items():
-        for meal_type, meal in day_meals.items():
-            display_meal(day, meal_type, meal)
+    
+    if st.button('Generate Meal Calendar'):
+        weekly_meals = generate_weekly_meals()
+        display_meals(weekly_meals)
 
 if __name__ == "__main__":
     app()
